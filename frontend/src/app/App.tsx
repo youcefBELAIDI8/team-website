01c62d2f7904921ba2ba80cf152a0c6599bfcd38
youcefBELAIDI8 <belaidi-youcef@hotmail.com>
2021-09-15 20:58:09 +0200

    fix bug - affichage du home à la connexion

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -10,16 +10,16 @@ function App() {
       <div className="App">
         <nav> 
           <ul>
-            <li><Link to="/home">Aceuil</Link></li>
+            <li><Link to="/">Aceuil</Link></li>
             <li><Link to="/about">Qui sommes-nous</Link></li>
             <li><Link to="/contact">Nous contacter</Link></li>
           </ul>
         </nav>
 
         <Switch>
-          <Route path="/home" component={HomePage} />  
           <Route path="/about" component={AboutPage} />  
           <Route path="/contact" component={ContactPage} />  
+          <Route path="/" component={HomePage} />  
         </Switch>        
       </div>
     );
